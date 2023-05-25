0b1b5534d3f4217cdc71212c7a1d3f98eda2d701
DFIT Ольга <Olga.Motovitsa@gmail.com>
2023-05-25 17:47:24 +0300

    add Packages + Homework1

---
+++ src/main/java/Homeworks/HW1/HW1Main.java
@@ -0,0 +1,9 @@
+package Homeworks.HW1;
+
+import Homeworks.HW1.Task1;
+public class HW1Main {
+    public static void main(String[] args) {
+        Task1 task1 = new Task1();
+        task1.TriangleNumber(0);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,18 @@
-//import Lessons.Lesson1.Task1;
-//import Lessons.Lesson1.Task2;
+import Lessons.Lesson1.Task1;
+import Lessons.Lesson1.Task2;
+import Lessons.Lesson1.model.Human;
 
-/*public class Main {
-    public static void main(String[] args) {*/
-/*      Human human = new Human("Alex");
-        System.out.println(human.getName());
-        human.setName("Mary");
-        System.out.println(human.getName());*/
-
-/*        Task1 task1 = new Task1();
-        task1.revers("Добро пожаловать на курс по Java");
-        Task2 hello = new Task2();
-        hello.hello();*/
-
-import Homeworks.HW1.Task1;
 public class Main {
     public static void main(String[] args) {
+        Human human = new Human("Alex");
+        System.out.println(human.getName());
+        human.setName("Mary");
+        System.out.println(human.getName());
+
         Task1 task1 = new Task1();
-        task1.TriangleNumber(0);
+        task1.revers("Добро пожаловать на курс по Java");
+        Task2 hello = new Task2();
+        hello.hello();
     }
 }
+
